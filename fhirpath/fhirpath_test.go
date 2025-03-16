f56abbc6ba63e58dc10d10c61d90677269352e3e
Lukas Schmierer <lukas.schmierer@damedic.ai>
2025-03-16 18:15:13 +0100

    adjust test for precedence

--- fhirpath/fhirpath_test.go
+++ fhirpath/fhirpath_test.go
@@ -11,6 +11,26 @@ import (
 	"testing"
 )
 
+// overrides for errors in the test xml
+var testOverrides = map[string]testdata.FHIRPathTest{
+	"testDateNotEqual": {
+		Output: []testdata.FHIRPathTestOutput{{
+			OutputType: "boolean",
+			Output:     "true",
+		}},
+	},
+	"testPrecedence3": {
+		Expression: testdata.FHIRPathTestExpression{
+			Expression: "(1 > 2) is Boolean",
+		},
+	},
+	"testPrecedence4": {
+		Expression: testdata.FHIRPathTestExpression{
+			Expression: "(1 | 1) is Integer",
+		},
+	},
+}
+
 func TestFHIRPathTestSuiteR4(t *testing.T) {
 	ctx := r4.Context()
 	ctx = fhirpath.WithAPDContext(ctx, apd.BaseContext.WithPrecision(100))
@@ -28,6 +48,16 @@ func TestFHIRPathTestSuiteR4(t *testing.T) {
 				name := test.Name
 				if test.Description != "" {
 					name = fmt.Sprintf("%s (%s)", name, test.Description)
+				}
+
+				override, ok := testOverrides[name]
+				if ok {
+					if override.Output != nil {
+						test.Output = override.Output
+					}
+					if override.Expression != (testdata.FHIRPathTestExpression{}) {
+						test.Expression = override.Expression
+					}
 				}
 
 				t.Run(name, func(t *testing.T) {
@@ -53,11 +83,6 @@ func TestFHIRPathTestSuiteR4(t *testing.T) {
 					assert.NoError(t, err)
 
 					expected := test.OutputCollection()
-					if name == "testDateNotEqual" {
-						// test is missing output
-						expected = fhirpath.Collection{fhirpath.Boolean(true)}
-					}
-
 					diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
 						A:        difflib.SplitLines(expected.String()),
 						B:        difflib.SplitLines(result.String()),
